fcf0ea0ed866f8b228497175ccaae09cd9f716b4
simon <simon@localhost>
2019-08-18 00:26:28 +0200

    Fix warnings

--- src/bin/enotes_systray.c
+++ src/bin/enotes_systray.c
@@ -8,13 +8,24 @@
 #define WIDTH  320
 #define HEIGHT 160
 
+void 
+_open_settings_systray(void *data, Evas_Object *obj, void *event_info)
+{
+   _open_settings(NULL, NULL, NULL, NULL);
+}
+
+void 
+enotes_win_help_systray(void *data, Evas_Object *obj, void *event_info)
+{
+   enotes_win_help(NULL, NULL, NULL, NULL);
+}
 
 static Eina_Bool
 _ev_handler(void *data EINA_UNUSED,
             int type EINA_UNUSED,
             void *event EINA_UNUSED)
 {
-   Eina_Bool ret = EINA_FALSE;
+   Eina_Bool ret;
 
 //    printf("systray ready event\n");
 
@@ -30,19 +41,12 @@ _ev_handler(void *data EINA_UNUSED,
 }
 
 
-static void
-_menu_clicked(void *data,
-            Evas_Object *obj EINA_UNUSED,
-            void *event_info EINA_UNUSED)
-{
-//    printf("Menu Item Clicked: %s\n", (const char *) data);
-}
 
 void
 enotes_systray()
 {
    Evas_Object *win, *ic;
-   Elm_Object_Item *it1 = NULL , *it2, *it3;
+   Elm_Object_Item *it1 = NULL;
    
 
 //    Eina_Bool init_ok = elm_need_systray();
@@ -83,69 +87,13 @@ enotes_systray()
       elm_object_item_part_content_set(it1, NULL, ic);
 //     eina_strbuf_free(buf2);
    elm_menu_item_separator_add(it, NULL);
-   
-//    it2 = elm_menu_item_add(it, NULL, NULL, "Categories to show", NULL, NULL);
-//    
-//    //// START RADIO TEST
-//    
-//    Elm_Object_Item *menu_it2;
-//    Evas_Object *radio;
-// 
-//    radio = elm_radio_add(it);
-//    elm_radio_state_value_set(radio, 0);
-//    elm_radio_value_set(radio, 0);
-//    elm_object_text_set(radio, "radio in menu");
-//    menu_it3 = elm_menu_item_add(it, it2, NULL, "SIMON", NULL, NULL);
-//    
-//       elm_object_item_part_text_set(menu_it3, NULL, "show all notes");
-   
-//    elm_object_item_content_set(menu_it3, radio);
-   
-   //// END RADIO TEST
-   
-   
-//    Eina_List *l;
-// //    cat_list = elm_list_selected_items_get(li);
-//    Elm_Object_Item *lit;
-//    elm_menu_item_add(it, it2, NULL, "Default",  _menu_clicked, "Default");
-//    EINA_LIST_FOREACH(cat_list, l, lit)
-//    {
-//    printf("Item clicked. %s is selected\n", elm_object_item_text_get(lit));
-//    elm_menu_item_add(it, it2, NULL, elm_object_item_text_get(lit),  _menu_clicked, elm_object_item_text_get(lit));
-//    //TODO: ABGLEICH MIT NOTE_LIST nach Übereinstimmung der CATEGORIES
-// //    elm_list_item_selected_get(lit);
-//    }
-//    
-   /// START LIST TEST
-//    elm_menu_item_add(it, NULL, NULL, NULL, NULL, NULL);
-//    
-//    Evas_Object *list;
-//                   Elm_Object_Item *li, *li1;
-//                      list = elm_list_add(it);
-//                   elm_list_multi_select_set(list, EINA_TRUE);
-//                   elm_list_multi_select_mode_set(list, ELM_OBJECT_MULTI_SELECT_MODE_DEFAULT);
-//                   evas_object_size_hint_weight_set(list, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-//                   evas_object_size_hint_align_set(list, EVAS_HINT_FILL, EVAS_HINT_FILL);
-//                   elm_list_mode_set(list, ELM_LIST_COMPRESS);
-//                   
-//                   elm_list_item_append(list, "Default", NULL, NULL, NULL, list);
-//                   li = elm_list_item_append(list, NULL, NULL, NULL, NULL, NULL);
-//                   elm_list_item_separator_set(li, EINA_TRUE);
-//                   elm_list_item_append(list, "Privat", NULL, NULL, NULL, list);
-//                   elm_list_item_append(list, "Work", NULL, NULL, NULL, list);
-//                   elm_list_item_append(list, "EJW FDS", NULL, NULL, NULL, list);
-//                   elm_list_item_append(list, "Pfalzgrafenweiler", NULL, NULL, NULL, list);
-//                   li1 = elm_list_item_append(list, "Enotes", NULL, NULL, NULL, list);
-//                   elm_list_go(list);
-//                   
-//    elm_object_item_content_set(it2, list);
-   /// END LIST TEST
+ 
    
    elm_menu_item_separator_add(it, NULL);
    
    elm_menu_item_add(it, NULL, "enotes", "New Note", _enotes_new, NULL); // TODO FÜR ALLE DREI: FUNKTIONS VORGABEN BEACHTEN
-   elm_menu_item_add(it, NULL, "media-eject", "Settings", _open_settings, NULL);
-   elm_menu_item_add(it, NULL, "help-about", "Help", enotes_win_help, NULL);
+   elm_menu_item_add(it, NULL, "media-eject", "Settings", _open_settings_systray, NULL);
+   elm_menu_item_add(it, NULL, "help-about", "Help", enotes_win_help_systray, NULL);
    
    elm_menu_item_add(it, NULL, "window-close", "Close Enotes", _enotes_exit, NULL);
 
